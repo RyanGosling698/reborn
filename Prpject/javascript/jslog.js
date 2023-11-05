9d0bbf35b56e3664b44f255bc2876893edd61416
RyanGosling698 <139676465+RyanGosling698@users.noreply.github.com>
2023-11-05 14:45:09 +0500

    Add files via upload

---
+++ Prpject/javascript/jslog.js
@@ -0,0 +1,61 @@
+document.getElementById("open-modal-btn").addEventListener("click",function(){
+    document.getElementById("modal-window").classList.add("open")
+})
+
+document.getElementById("close-modul-window").addEventListener("click",function(){
+    document.getElementById("modal-window").classList.remove("open")
+})
+
+document.getElementById("open-modal-btn2").addEventListener("click",function(){
+    document.getElementById("modal-window2").classList.add("open")
+})
+
+document.getElementById("close-modul-window2").addEventListener("click",function(){
+    document.getElementById("modal-window2").classList.remove("open")
+})
+
+
+
+// Закрыть модальное окно при нажатии на клавишу "Esc"
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        document.getElementById("modal-window2").classList.remove("open")
+        document.getElementById("modal-window").classList.remove("open")
+        document.getElementById("password").value = "";
+        document.getElementById("password__2").value = "";
+        document.getElementById("password__3").value = "";
+        document.getElementById("email__1").value = "";
+        document.getElementById("email").value = "";
+    }
+});
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        document.getElementById("modal-window2").classList.remove("open")
+        document.getElementById("modal-window").classList.remove("open")
+        document.getElementById("password").value = "";
+        document.getElementById("password__2").value = "";
+        document.getElementById("password__3").value = "";
+        document.getElementById("email__1").value = "";
+        document.getElementById("email").value = "";
+    }
+});
+
+
+
+function closeModal() {
+    modal.classList.remove('open');
+    // Удалить модальное окно из DOM
+    modal.remove();
+}
+
+
+
+
+let burger = document.querySelector('.burger-menu')
+let nav = document.querySelector('.main-nav')
+
+burger.addEventListener('click', function() {
+    burger.classList.toggle('active')
+    nav.classList.toggle('active') 
+    
+})
